76b424be6f9a5ca51bf1662f0a4b59b26ff053e1
iquhsek <shuqike.research@gmail.com>
2026-01-29 21:27:00 -0500

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://shuqike.github.io",
-  base: "/website",
+  site: "https://shuqike.com",
 });
